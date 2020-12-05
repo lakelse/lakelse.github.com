3d82b0bb5a6e5b4863945122fc8478ec0dcd4d7f
Jonathan MacDonald <jonathan.macdonald@gmail.com>
2020-12-05 17:21:05 -0600

    Update install_tmux.sh

--- install_tmux.sh
+++ install_tmux.sh
@@ -2,7 +2,7 @@
 # install: \curl -sSL https://raw.githubusercontent.com/lakelse/lakelse.github.com/master/install_tmux.sh | bash
 set -e
 
-sudo dnf install tmux -y
+# sudo dnf install tmux -y
 
 declare -A c=( \
       [curl]="/usr/bin/curl --remote-name --silent" \
